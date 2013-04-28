3d5fb51408b4067c5329fbf5aff75b2fa0c73ebe
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2013-04-28 16:31:49 -0700

    Initial Commit

---
+++ xcs/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xcs
+//
+//  Created by Oleksandr Tymoshenko on 2013-04-28.
+//  Copyright (c) 2013 Bluezbox Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
